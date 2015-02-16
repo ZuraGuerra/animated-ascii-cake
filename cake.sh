78fbe6cf8903f9261ef7c27e0d39d34ddcf3dcf8
Zurafa Guerra <zuraguerra@Mac-mini.local>
2015-02-16 01:15:33 -0600

    not working, h8 my life

---
+++ cake.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+cake1()
+{
+	echo "           ~                  ~"
+	echo "     +                   +                +       +" 
+	echo "                  +               +"
+	echo "  ~       +                +         ~    +          "
+	echo "              +       ~        +              +   ~"
+	echo "                  )         (         )              +"
+	echo "    +    ~     ) (_)   (   (_)   )   (_) (  +"
+	echo "           +  (_) º ) (_) ) º ( (_) ( º (_)       +"
+	echo "              _º,-º(_)-º-(_)º(_)-º-(_)º-,º_    "
+	echo "  +         ,' º  º º  º  º º º  º  º º  º `,   ~     +"
+	echo "           ;   º    º  º  º   º  º  º    º   ;   "
+	echo "    ~      ;,       º     º   º     º       ,;      +"
+	echo "        +  l `-,__                     __,-' l +"
+	echo "           l      `````````````````````      l         +"
+	echo "     +     l                                 l    "
+	echo "           l                                 l       ~"
+	echo "   ~   +   l                                 l + "
+	echo "           l                                 l         +"
+	echo "   +    _,-l                                 l-,_  "
+	echo "      ,'   ',      ~            ~           ,'   `,  +"
+	echo "      ;      `-,__                     __,-'      ;"
+	echo "       `,         `````````````````````         ,'"
+	echo "         `-,_                             _,-'"
+	echo "              `````````-----------`````````"
+
+}
+cake2()
+{
+	echo "           ~                  ~"
+	echo "     +                   +                +       +" 
+	echo "                  +               +"
+	echo "  ~       +                +         ~    +          "
+	echo "              +       ~        +              +   ~"
+	echo "                  (         )         (              +"
+	echo "    +    ~     ( (_)   )   (_)   (   (_) )  +"
+	echo "           +  (_) º ( (_) ( º ) (_) ) º (_)       +"
+	echo "              _º,-º(_)-º-(_)º(_)-º-(_)º-,º_    "
+	echo "  +         ,' º  º º  º  º º º  º  º º  º `,   ~     +"
+	echo "           ;   º    º  º  º   º  º  º    º   ;   "
+	echo "    ~      ;,       º     º   º     º       ,;      +"
+	echo "        +  l `-,__                     __,-' l +"
+	echo "           l      `````````````````````      l         +"
+	echo "     +     l                                 l    "
+	echo "           l                                 l       ~"
+	echo "   ~   +   l                                 l + "
+	echo "           l                                 l         +"
+	echo "   +    _,-l                                 l-,_  "
+	echo "      ,'   ',      ~            ~           ,'   `,  +"
+	echo "      ;      `-,__                     __,-'      ;"
+	echo "       `,         `````````````````````         ,'"
+	echo "         `-,_                             _,-'"
+	echo "              `````````-----------`````````"
+
+}
+echo -e "\n\n\n\n\n\n\n\n\n\033[8F\033[s"
+while [ 1 = 1 ]
+do  echo -e "\033[u           FE          " ; cake1
+    echo -e "\033[u          FELI         " ; cake2
+    echo -e "\033[u         FELIZ         " ; cake1
+    echo -e "\033[u       FELIZ CU        " ; cake2
+    echo -e "\033[u      FELIZ CUMP       " ; cake1
+    echo -e "\033[u    FELIZ CUMPLE       " ; cake2
+    echo -e "\033[u   FELIZ CUMPLE,       " ; cake1
+    echo -e "\033[u  FELIZ CUMPLE, SA     " ; cake2
+    echo -e "\033[u FELIZ CUMPLE, SAM!    " ; cake1
+    echo -e "\033[u FELIZ CUMPLE, SAM!    " ; cake2
+    echo -e "\033[u FELIZ CUMPLE, SAM!    " ; cake1
+    echo -e "\033[u FELIZ CUMPLE, SAM!    " ; cake2
+done
